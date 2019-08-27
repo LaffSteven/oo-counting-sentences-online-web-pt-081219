a0c07d73d6d7dbbc96e69dc78578905f6c3d01b8
Steven Laff <steve.laff@gmail.com>
2019-08-27 16:56:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,24 +1,21 @@
 require 'pry'
 
 class String
-  
-  def initialize
-    
-  end
+
 
   def sentence?
-    return @self.end_with?(".") ? true : false
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    
   end
 end
